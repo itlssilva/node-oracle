8b2ad05ed103e55d571b80d8c71180dbe0812472
Italo Petrucio dos Santos Silva <i.petrucio@btp.com.br>
2019-03-26 09:38:09 -0300

    Conexão e consulta a base Oracle

--- scr/app.js
+++ scr/app.js
@@ -9,28 +9,59 @@ const dbConfig = require('./dbconfig.js');
 
 oracledb.getConnection(
     {
-    user : dbConfig.user,    
-    password : dbConfig.password,
-    connectString : dbConfig.connectString
+        user: dbConfig.user,
+        password: dbConfig.password,
+        connectString: dbConfig.connectString
     },
-    
-    function(err, connection){
-    if (err) {    
-    console.error(err.message);
-    return;    
+    function (err, connection) {
+        if (err) {
+            console.error(err.message);
+            return;
+        }
+        connection.execute(
+            "SELECT * FROM ACCESSCONTROL.USUARIO USUA WHERE USUA.USUA_ID = 657", function (err, result) {
+                if (err) {
+                    console.error(err.message);
+                    doRelease(connection);
+                    return;
+                }
+                //console.log(result.metaData); // [ { name: ‘SYSDATE’ } ]
+                console.log(result.rows); // [ [ 2017-12-08T19:03:45.000Z ] ]
+                doRelease(connection);
+            });
+    }); function doRelease(connection) {
+        connection.close(
+            function (err) {
+                if (err) {
+                    console.error(err.message);
+                }
+            });
     }
+
+// oracledb.getConnection(
+//     {
+//     user : dbConfig.user,    
+//     password : dbConfig.password,
+//     connectString : dbConfig.connectString
+//     },
     
-    console.log('Connection was successful!');
+//     function(err, connection){
+//     if (err) {    
+//     console.error(err.message);
+//     return;    
+//     }
     
-    connection.close(
+//     console.log('Connection was successful!');
+
+//     connection.close(
     
-    function(err) {   
-    if (err) {    
-    console.error(err.message);
-    return;    
-    }    
-    });
- });
+//     function(err) {   
+//     if (err) {    
+//     console.error(err.message);
+//     return;    
+//     }    
+//     });
+//  });
 
 
 const route = router.get('/', (req, res, next) => {
--- scr/dbconfig.js
+++ scr/dbconfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-    user : /*process.env.NODE_ORACLEDB_USER ||*/ "<YOUR DATABASE USER HERE>",
-    password : /*process.env.NODE_ORACLEDB_PASSWORD ||*/ "<YOUR DATABASE PASSWORD HERE>",
-    connectString : /*process.env.NODE_ORACLEDB_CONNECTIONSTRING ||*/ "<YOUR DATABASE CONNECT STRING HERE>",
+    user : /*process.env.NODE_ORACLEDB_USER ||*/ "SUPORTE",
+    password : /*process.env.NODE_ORACLEDB_PASSWORD ||*/ "SUPORTE",
+    connectString : /*process.env.NODE_ORACLEDB_CONNECTIONSTRING ||*/ "DBD2OPR"// "Data Source=(DESCRIPTION = (ADDRESS = (PROTOCOL = TCP)(HOST = sv09d-dbs.braporto.local)(PORT = 1521)) (CONNECT_DATA = (SID = DBD6OPR)))",
     };
